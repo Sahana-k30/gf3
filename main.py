9b8c39bde575e475ba8319159ba24ee1a48acdf9
Sahana <sahana.k30may@gmail.com>
2026-04-21 22:48:51 +0530

    psuhing the code

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app.include_router(router, prefix="/api")
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=9000, reload=True)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\admin\Desktop\GF3 demo\venv
+set VIRTUAL_ENV=C:\Users\admin\Desktop\gf3_new\gf3\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
